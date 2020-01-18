e28aff882536f1b21ed5bf257629503c99c1ef69
shumatsuura <shunsuke.mtr@gmail.com>
2020-01-18 13:56:22 +0900

    DBをSQLからpostgresqlに変更

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,13 @@
 
 ActiveRecord::Schema.define(version: 2020_01_17_121630) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "feeds", force: :cascade do |t|
     t.text "image"
     t.text "content"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_feeds_on_user_id"
@@ -29,4 +32,5 @@ ActiveRecord::Schema.define(version: 2020_01_17_121630) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "feeds", "users"
 end
